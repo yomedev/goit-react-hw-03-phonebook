7e1e394ad9f558938c99be0c069df5e89f22b87f
Yaroslav <yomelianenkowork@gmail.com>
2022-09-14 19:33:37 +0300

    added local storage module 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,23 @@ import { nanoid } from 'nanoid'
 import ContactForm from "./contactForm/ContactForm";
 import Filter from "./filter/Filter";
 import ContactList from "./contactList/ContactList";
+import { useEffect } from "react";
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ])
+  const [contacts, setContacts] = useState([])
 
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    const contactsFromLS = JSON.parse(localStorage.getItem('contacts'))
+    if (contactsFromLS) {
+      setContacts(contactsFromLS)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
 
   const addContact = (name, number) => {
     if (contacts.some(contact => contact.name === name)) {
